7dacb1f22fba0bc007ac157e2f91b46242e02248
Rodrigo Retamal <alonsoretamal27@gmail.com>
2026-08-28 15:01:24 -0400

    Se agrego el requerimiento b4, cancelacion de la maquina

--- src/punto_flotante.py
+++ src/punto_flotante.py
@@ -1,14 +1,10 @@
 import numpy as np
 
-def ejercicio_b1():
+def mantisa_corta_b1():
     
     print("B1: Cifras significativas y mantisa corta")
-   
-    
     valor_real = 1000.76 #valor real del dataset
-   
     valor_aprox_3cifras = 1000.0 #queda asi porque solo se usan 3 cifras significativas, truncando la mantisa, seria 1.00 x 10**3
-    
     ea = abs(valor_real - valor_aprox_3cifras) # calculo del error absoluto
     er = (ea / valor_real) * 100 # calculo del error relativo porcentual
     
@@ -21,5 +17,28 @@ def ejercicio_b1():
     print("en el estándar IEEE 754 de punto flotante, descartando los bits de menor")
     print("peso y generando un error de representación intrínseco.")
 
+def cancelacion_maquina_b4():
+  
+   
+    print("B4: Cancelación en la máquina")
+    
+
+    p_dic2023 = 874.67 #numeros casi identicos
+    p_dic2022 = 875.66
+    valor_teorico = p_dic2023 - p_dic2022  # = -0.99
+
+        #Este bloque combierte ambos numeros al estandar IEEE 754 de precision simple
+    f32_dic2023 = np.float32(p_dic2023) 
+    f32_dic2022 = np.float32(p_dic2022)
+    resta_f32 = f32_dic2023 - f32_dic2022
+
+        #Este bloque combierte ambos numeros al estandar IEEE 754 de precision doble
+    f64_dic2023 = np.float64(p_dic2023)
+    f64_dic2022 = np.float64(p_dic2022) 
+    resta_f64 = f64_dic2023 - f64_dic2022
+    print(f"Diferencia teórica exacta : {valor_teorico:.6f}")
+    print(f"Resta en float32          : {resta_f32:.8f}")
+    print(f"Resta en float64          : {resta_f64:.16f}")
 if __name__ == "__main__":
-    ejercicio_b1()
+    mantisa_corta_b1()
+    cancelacion_maquina_b4()
